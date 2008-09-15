4e46e683817872cbedac2f451c465cefedbdc2a4
sam.saffron <sam.saffron@096cc86f-8a4c-0410-9b85-8b5e0eee1c45>
2008-09-15 07:56:45 +0000

    Test file and exclusions
    git-svn-id: https://videobrowser.googlecode.com/svn/trunk@65 096cc86f-8a4c-0410-9b85-8b5e0eee1c45

---
+++ TestVideoBrowser/ConfigTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SamSoft.VideoBrowser.Util;
+
+namespace TestVideoBrowser
+{
+    [TestFixture]
+    public class ConfigTest
+    {
+        [Test]
+        public void Test()
+        {
+           // Console.WriteLine( Updater.AssemblyVersion);
+           // Assert.AreEqual(new Version(Updater.AssemblyVersion).ToString() , Updater.AssemblyVersion);
+        }
+   }
+}
