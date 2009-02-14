98b1ee53fcdb6f6906ce255693072a5e6a150837
paul@haleynet.co.uk <paul@haleynet.co.uk@096cc86f-8a4c-0410-9b85-8b5e0eee1c45>
2009-02-14 14:42:58 +0000

    trunk: add routine to verify and patch CustomSyles.mcml and CustomFonts.mcml file if they are missing entries.
    git-svn-id: https://videobrowser.googlecode.com/svn/trunk@651 096cc86f-8a4c-0410-9b85-8b5e0eee1c45

--- MediaBrowser/Code/Launch.cs
+++ MediaBrowser/Code/Launch.cs
@@ -6,11 +6,13 @@ using System.IO;
 using System;
 using System.Threading;
 using MediaBrowser.LibraryManagement;
+using System.Xml;
 
 namespace MediaBrowser
 {
     public class MyAddIn : IAddInModule, IAddInEntryPoint
     {
+        private const string CUSTOM_STYLE_FILE = "CustomStyles.mcml";
 
         public void Initialize(Dictionary<string, object> appInfo, Dictionary<string, object> entryPointInfo)
         {
@@ -41,14 +43,14 @@ namespace MediaBrowser
                 Trace.Listeners.Add(t);
             }
             Environment.CurrentDirectory = Helper.AppDataPath;
-            SetupStylesMcml();
-            SetupFontsMcml();
+            SetupStylesMcml(host);
+            SetupFontsMcml(host);
 
             Application app = new Application(new MyHistoryOrientedPageSession(), host);
             app.GoToMenu();
         }
 
-        private void SetupFontsMcml()
+        private void SetupFontsMcml(AddInHost host)
         {
             try
             {
@@ -65,6 +67,10 @@ namespace MediaBrowser
                 if (File.Exists(custom))
                 {
                     Trace.TraceInformation("Using custom fonts mcml");
+                    if (VerifyStylesXml(custom, Resources.FontsDefault))
+                    {
+                        host.MediaCenterEnvironment.Dialog("CustomFonts.mcml as been pathed with missing values", "CustomFonts.mcml", DialogButtons.Ok, 100, true);
+                    }
                     File.Copy(custom, file);
                 }
                 else
@@ -87,12 +93,12 @@ namespace MediaBrowser
             }
         }
 
-        private void SetupStylesMcml()
+        private void SetupStylesMcml(AddInHost host)
         {
             try
             {
                 string file = Path.Combine(Helper.AppDataPath, "Styles_DoNotEdit.mcml");
-                string custom = Path.Combine(Helper.AppDataPath, "CustomStyles.mcml");
+                string custom = Path.Combine(Helper.AppDataPath, CUSTOM_STYLE_FILE);
                 if (File.Exists(file))
                 {
                     try
@@ -104,6 +110,10 @@ namespace MediaBrowser
                 if (File.Exists(custom))
                 {
                     Trace.TraceInformation("Using custom styles mcml");
+                    if (!VerifyStylesXml(custom, Resources.StylesDefault))
+                    {
+                        host.MediaCenterEnvironment.Dialog(CUSTOM_STYLE_FILE + " has been patched with missing values", CUSTOM_STYLE_FILE, DialogButtons.Ok, 100, true);
+                    }
                     File.Copy(custom, file);
                 }
                 else
@@ -132,6 +142,35 @@ namespace MediaBrowser
                 Trace.TraceError("Error creating Styles_DoNotEdit.mcml\n" + ex.ToString());
             }
 
+        }
+
+
+        private bool VerifyStylesXml(string filename, byte[] resource)
+        {
+            XmlDocument custom = new XmlDocument();
+            custom.Load(filename);
+            XmlDocument def = new XmlDocument();
+            using (MemoryStream ms = new MemoryStream(resource))
+            {
+                def.Load(ms);
+            }
+            List<XmlNode> missingNodes = new List<XmlNode>();
+            foreach(XmlNode node in def.SelectNodes("//*[@Name]"))
+            {
+                if (custom.SelectSingleNode(string.Format("//*[@Name='{0}']", node.Attributes["Name"].Value)) == null)
+                    missingNodes.Add(node);
+                    //return string.Format("Missing entry in {0} for\n{1}", filename,node.OuterXml);
+            }
+            if (missingNodes.Count > 0)
+            {
+                foreach (XmlNode n in missingNodes)
+                {
+                    custom.DocumentElement.AppendChild(custom.ImportNode(n, true));
+                }
+                custom.Save(filename);
+                return false;
+            }
+            return true;
         }
     }
 }
