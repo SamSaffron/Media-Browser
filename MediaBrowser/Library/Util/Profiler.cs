a1e553d9948c21b2e52f07bc4640412b5403d6f0
sam.saffron <sam.saffron@096cc86f-8a4c-0410-9b85-8b5e0eee1c45>
2009-03-07 11:27:30 +0000

    Conclusion for today, svn sucks ...
    git-svn-id: https://videobrowser.googlecode.com/svn/trunk@759 096cc86f-8a4c-0410-9b85-8b5e0eee1c45

---
+++ MediaBrowser/Library/Util/Profiler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace MediaBrowser.Util
+{
+    class Profiler : IDisposable
+    {
+        string caller;
+        string name;
+        Stopwatch stopwatch;  
+
+        public Profiler(string name)
+        {
+            this.name = name;
+            StackTrace st = new StackTrace();
+            caller = st.GetFrame(1).GetMethod().Name;
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+        }
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            stopwatch.Stop();
+            Trace.WriteLine(caller + ":" + name + " took " + stopwatch.ElapsedMilliseconds.ToString() + " miliseconds!"); 
+        }
+
+        #endregion
+    }
+}
