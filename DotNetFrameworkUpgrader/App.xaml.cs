6118172fd8810b1a7c6a45f96ec8cf9e309f8a7d
sam.saffron <sam.saffron@096cc86f-8a4c-0410-9b85-8b5e0eee1c45>
2009-03-10 22:42:07 +0000

    Added dot net 3.5 bootstrapper
    git-svn-id: https://videobrowser.googlecode.com/svn/trunk@782 096cc86f-8a4c-0410-9b85-8b5e0eee1c45

---
+++ DotNetFrameworkUpgrader/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DotNetFrameworkUpgrader {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
