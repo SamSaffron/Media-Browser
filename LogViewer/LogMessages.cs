3b1f42b6ea2d21d7be98ee659cde8903e12ef4fe
sam.saffron <sam.saffron@096cc86f-8a4c-0410-9b85-8b5e0eee1c45>
2009-04-30 10:31:42 +0000

    ignore a few dirs, missing files from checkin
    git-svn-id: https://videobrowser.googlecode.com/svn/trunk@1048 096cc86f-8a4c-0410-9b85-8b5e0eee1c45

---
+++ LogViewer/LogMessages.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Logging;
+using System.IO;
+using System.Collections.ObjectModel;
+using MediaBrowser.Library.Threading;
+using MediaBrowser.Library.Extensions;
+using System.Windows.Threading;
+using System.Timers;
+
+namespace LogViewer {
+    class LogMessages : ObservableCollection<LogRow> {
+
+        string path;
+
+        HashSet<Guid> rowHashs = new HashSet<Guid>();
+        System.Threading.Timer timer;
+        Dispatcher dispatcher; 
+
+        // used by the UI
+        public LogMessages() {
+            Add(new LogRow()
+            {
+                Category = "cat1",
+                Message = "This is my log message \n its a long one containing lots of info",
+                Severity = LogSeverity.Verbose,
+                ThreadId = 22,
+                ThreadName = "thready",
+                Time = DateTime.Now
+            });
+
+            Add(new LogRow()
+            {
+                Category = "cat1",
+                Message = "This is my log message \n its a long one containing lots of info",
+                Severity = LogSeverity.Info,
+                ThreadId = 22,
+                ThreadName = "thready",
+                Time = DateTime.Now
+            });
+
+            Add(new LogRow()
+            {
+                Category = "cat1",
+                Message = "This is my log message \n its a long one containing lots of info",
+                Severity = LogSeverity.Warning,
+                ThreadId = 22,
+                ThreadName = "thready",
+                Time = DateTime.Now
+            });
+            
+            Add(new LogRow()
+            {
+                Category = "cat1",
+                Message = "This is my log message \n its a long one containing lots of info",
+                Severity = LogSeverity.Error,
+                ThreadId = 22,
+                ThreadName = "thready",
+                Time = DateTime.Now
+            });
+        }
+
+        public LogMessages(string path) {
+            this.path = path;
+
+            // really expensive should be a log watcher
+            timer = Async.Every(2000, LoadMessages);
+            dispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        private void LoadMessages() {
+
+            var newRows = new List<LogRow>();
+
+            foreach (var file in Directory.GetFiles(path)) {
+                foreach (var line in GetLines(file)) {
+                    LogRow row = LogRow.FromString(line);
+                    var hash = row.ToString().GetMD5();
+                    if (!rowHashs.Contains(hash)) {
+                        newRows.Add(row);
+                        rowHashs.Add(hash);
+                    }
+                }
+            }
+
+            dispatcher.Invoke((Action)(() =>
+            {
+                foreach (var row in newRows.OrderBy(row => row.Time)) {
+                    Add(row);
+                }
+            }));
+
+
+        }
+
+        private IEnumerable<string> GetLines(string filename) {
+            List<string> lines = new List<string>();
+            try {
+                using (var stream = new FileStream(filename,FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var reader = new StreamReader(stream);
+                    while (!reader.EndOfStream)
+                    {
+                        lines.Add(reader.ReadLine());
+                    } 
+                }
+            } catch { 
+                // dont care
+            }
+
+            return lines;
+        }
+    }
+}
---
+++ LogViewer/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MediaBrowser.Library.Configuration;
+using MediaBrowser.Library.Logging;
+
+namespace LogViewer {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+
+        LogMessages logMessages = new LogMessages(ApplicationPaths.AppLogPath);
+
+        public Window1() {
+            InitializeComponent();
+            listView.ItemsSource = logMessages;
+
+            listView.SelectionChanged += new SelectionChangedEventHandler(ListViewSelectionChanged);
+            logMessages.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(LogMessagesCollectionChanged);
+        }
+
+        void LogMessagesCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) {
+            listView.ScrollIntoView(listView.Items[listView.Items.Count - 1]);
+        }
+
+        void ListViewSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            textBox.Text = ((LogRow)listView.SelectedItem).FullDescription; 
+        }
+    }
+}
