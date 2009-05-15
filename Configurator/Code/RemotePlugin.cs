0d1e57146ccc80cc953a893be47949065ccf194d
Sam Saffron <sam.saffron@gmail.com>
2009-05-15 17:43:01 +1000

    Basic support for installing plugins from the mediabrowser.tv website and other places.
    Needs more error handling

---
+++ Configurator/Code/RemotePlugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaBrowser.Library.Plugins;
+
+namespace Configurator.Code {
+    class RemotePlugin : IPlugin {
+
+        public void Init(MediaBrowser.Library.Kernel kernel) {
+        }
+
+        public string Filename {
+            get;
+            set;
+        }
+
+        public string Name {
+            get;
+            set;
+        }
+
+        public string Description {
+            get;
+            set;
+        }
+
+        public System.Version Version {
+            get;
+            set;
+        }
+
+        public string BaseUrl {
+            get;
+            set;
+        } 
+    }
+}
